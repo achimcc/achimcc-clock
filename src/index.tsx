b10ff8cc38bfdc6979f8498d1083946516fe5b5c
Achim <achim.schneider@equeo.de>
2020-12-07 22:38:12 +0100

    added clock

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
 import styles from './styles.module.css'
+import Clock from './clock'
 
 interface Props {
   text: string
 }
+
+export {Clock} 
 
 export const ExampleComponent = ({ text }: Props) => {
   return <div className={styles.test}>Example Component: {text}</div>
